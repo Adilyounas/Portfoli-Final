419cb558c1adc7ec018afcf185137877f5066a81
Muhammad Adil <addhtml00@gmail.com>
2023-07-26 12:35:21 +0500

    click me for demo

--- src/components/work/WorkItem.jsx
+++ src/components/work/WorkItem.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const WorkItem = ({ item }) => {
   return (
     <div className="work__card" key={item.id}>
-      <img src={item.image} alt="" className="work__img" />
+      <img src={item.image} alt={item.image} className="work__img" />
 
       <h3 className="work__title">{item.title}</h3>
       <a href={item.url} className="work__button" target="_blank"  rel="noreferrer">
-        Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+        Click Me For Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
       </a>
     </div>
   );
